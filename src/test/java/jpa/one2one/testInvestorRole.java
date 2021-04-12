4620860cc83516e0fd43b25442e5239e1c489e02
foxlo <foxlo@DESKTOP-4R8BSQ7>
2021-04-12 21:51:23 +0800

    project

--- src/test/java/jpa/one2one/testInvestorRole.java
+++ src/test/java/jpa/one2one/testInvestorRole.java
@@ -3,6 +3,7 @@ package jpa.one2one;
 import com.spring.project.portfolio.entities.Investor;
 import com.spring.project.portfolio.entities.Role;
 import com.spring.project.portfolio.entities.RoleType;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -23,14 +24,15 @@ public class testInvestorRole extends JPATemplate {
         // R - Read
         List<Investor> investors = findAll();
         investors.stream().forEach(System.out::println);
-//        // U - update
-//        updateRoleById(Optional.of(1), "VIP_C");
-//        Investor investor = findOneById(Optional.of(1));
-//
-//        // D - delete
-//        deleteById(Optional.of(2));
-//        Optional<Investor> investorNotExisted = findNotExistedById(Optional.of(2));
-//        Assert.assertFalse(investorNotExisted.isPresent());
+        // U - update
+        updateRoleById(Optional.of(1), RoleType.MEMBER);
+        Investor investor = findOneById(Optional.of(1));
+        Assert.assertEquals(RoleType.MEMBER, investor.getRoles().iterator().next().getRoleType());
+
+        // D - delete
+        deleteById(Optional.of(4));
+        Optional<Investor> investorNotExisted = findNotExistedById(Optional.of(4));
+        Assert.assertFalse(investorNotExisted.isPresent());
     }
 
     public void create() {
@@ -44,12 +46,14 @@ public class testInvestorRole extends JPATemplate {
         investor2.getRoles().add(roleOfUser);
         Investor investor3 = new Investor("Cathy", "cathy", "Cathy@gmail.com", 100000, Boolean.TRUE, new Date());
         investor3.getRoles().add(roleOfMEMBER);
+        Investor investor4 = new Investor("Delete", "delete", "Delete@gmail.com", 500000, Boolean.TRUE, new Date());
+        investor4.getRoles().add(roleOfMEMBER);
         
-        entityManager.persist(roleOfUser);
-        entityManager.persist(roleOfMEMBER);
+
         entityManager.persist(investor1);
         entityManager.persist(investor2);
         entityManager.persist(investor3);
+        entityManager.persist(investor4);
 
         entityManager.flush();
 
@@ -66,23 +70,25 @@ public class testInvestorRole extends JPATemplate {
     public Investor findOneById(Optional<Integer> id) {
         return entityManager.find(Investor.class, id.get());
     }
-//
-//    public void updateRoleById(Optional<Integer> id, String role) {
-//        Investor investor = entityManager.find(Investor.class, id.get());
-//        Query query = entityManager.createQuery("Select r from Role r WHERE r.name = ?1");
-//        query.setParameter(1, role);
-//        Role queryRole = (Role)query.getSingleResult();
-//        investor.setRole(queryRole);
-//        entityManager.persist(investor);
-//        entityManager.flush();
-//    }
-//
-//    public void deleteById(Optional<Integer> id) {
-//        Investor investor = entityManager.find(Investor.class, id.get());
-//        entityManager.remove(investor);
-//    }
-//
-//    public Optional<Investor> findNotExistedById(Optional<Integer> id) {
-//        return Optional.ofNullable(entityManager.find(Investor.class, id.get()));
-//    }
+
+    public void updateRoleById(Optional<Integer> id, RoleType roleType) {
+        Investor investor = entityManager.find(Investor.class, id.get());
+        Query query = entityManager.createQuery("Select r from Role r WHERE r.roleType = ?1");
+        query.setParameter(1, roleType);
+        Role queryRole = (Role)query.getSingleResult();
+        Set<Role> roles = new HashSet<>();
+        roles.add(queryRole);
+        investor.setRoles(roles);
+        entityManager.persist(investor);
+        entityManager.flush();
+    }
+
+    public void deleteById(Optional<Integer> id) {
+        Investor investor = entityManager.find(Investor.class, id.get());
+        entityManager.remove(investor);
+    }
+
+    public Optional<Investor> findNotExistedById(Optional<Integer> id) {
+        return Optional.ofNullable(entityManager.find(Investor.class, id.get()));
+    }
 }
